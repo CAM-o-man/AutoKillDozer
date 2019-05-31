f3a351e803d3854cb2de576da977d80f9004338b
raspberryPI <connor.mcdermid@mcvts.org>
2019-05-31 09:07:14 -0400

    added pi

---
+++ movement.py
@@ -0,0 +1,36 @@
+import RPi.GPIO as GPIO
+import time
+import firebase
+from firebase import firebase as fire
+
+firebase = firebase.FirebaseApplication('https://makers2018-e66f3.firebaseio.com', None)  # Firebase implemented
+result = firebase.get('path/', None)  # Get a value from a node
+
+
+servoPIN = 24
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPIN, GPIO.OUT)
+
+p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
+p.start(2.5) # Initialization
+try:
+  while True:
+    p.ChangeDutyCycle(5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(7.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(10)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(12.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(10)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(7.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(2.5)
+    time.sleep(0.5)
+except KeyboardInterrupt:
+  p.stop()
+  GPIO.cleanup()
